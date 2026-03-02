7a27d609c6c2dc051a203e78965c1c1b1da017ee
Mohamed Eldakroury <alaaeldakrory@gmail.com>
2026-03-02 16:41:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3744208193-a3aca",
+  "appId": "1:861266764019:web:bad24cc3bd503df8329786",
+  "apiKey": "AIzaSyCawIRyn2k_kXRnETt2xYF3mnMElqH55Xg",
+  "authDomain": "studio-3744208193-a3aca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "861266764019"
+};
